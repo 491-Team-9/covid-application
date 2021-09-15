f08ad0106d3034fae82058e4536e904225887b6b
Louella Caraang <lcaraang@hawaii.edu>
2021-09-14 22:52:44 -1000

    landing page

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Button, Card, Grid } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-      <Grid id='landing-page' verticalAlign='middle' textAlign='center' container>
-
-        <Grid.Column width={4}>
-          <Image size='small' circular src="/images/meteor-logo.png"/>
+      <Grid id='landing' textAlign='center'>
+        <Grid.Column width={5}>
+          <Card.Group itemsPerRow={1}>
+            <Card>
+              <Card.Content>
+                <Card.Header>
+                    Welcome to ProtectUH
+                </Card.Header>
+                <Card.Description>
+                    To help keep our campus safe, we are asking students, faculty, staff, and visitors to take a daily
+                    questionnaire.
+                  <br/><br/><Button href='#/home' color="blue">Continue</Button>
+                </Card.Description>
+              </Card.Content>
+            </Card>
+          </Card.Group>
         </Grid.Column>
-
-        <Grid.Column width={8}>
-          <h1>Welcome to this template</h1>
-          <p>Now get to work and modify this app!</p>
-        </Grid.Column>
-
       </Grid>
     );
   }
